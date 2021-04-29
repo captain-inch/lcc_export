f4f9660c19aec3f8fac25566922ac0bbeab39438
Etienne Pousse <etiennepousse@live.fr>
2021-04-29 17:10:07 +0200

    Replacing last commit (outdated). This version includes a proper lightbox with a module and prev / next feature. /Should be used with last commit version of gallery_content

--- src/components/wineyard/gallery.jsx
+++ src/components/wineyard/gallery.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import Gallery from "react-photo-gallery";
 import { gsap } from "gsap";
 import { gallerySettings } from "../../content/gallery_content.jsx";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 const body = document.querySelector("body");
 
 export default class MyGallery extends Component {
@@ -9,111 +11,76 @@ export default class MyGallery extends Component {
     super(props);
     this.state = {
       gallerySettings: gallerySettings,
-      active: false,
-      pos: null,
-      src: null,
+      lightboxSrcs: [],
+      lightboxActive: false,
+      lightboxKey: 0,
     };
     this.openLightbox = this.openLightbox.bind(this);
   }
   openLightbox(e) {
-    const init_pos = e.target.getBoundingClientRect();
-    if (!this.state.active) {
-      this.setState({ active: true, src: e.target.src, pos: init_pos }, () => {
-        gsap.to(body, { overflow: "hidden" }, 0);
-        gsap.to(
-          ".lightboxBg",
-          {
-            opacity: 1,
-            display: "block",
-            duration: 0.25,
-          },
-          0
-        );
-        gsap.fromTo(
-          ".lightboxImg",
-          {
-            top: 0,
-            left: 0,
-            x: init_pos.left,
-            y: init_pos.top,
-            scale: 0,
-          },
-          {
-            top: "50vh",
-            left: "50vw",
-            x: "-50%",
-            y: "-50%",
-            scale: 1,
-            duration: 0.25,
-          },
-          0
-        );
-        // maxWidth: "90vw",
-        //     maxHeight: "90vh",
-        // maxWidth: init_pos.width,
-        // maxHeight: init_pos.height,
-        // const maxX = 0.9;
-        // const maxY = 0.9;
-        // const scale = Math.min(
-        //   (window.innerWidth / e.target.width) * maxX,
-        //   (window.innerHeight / e.target.height) * maxY
-        // );
-        // gsap
-        //   .timeline()
-        //   .to(e.target, {
-        //     position: "fixed",
-        //     top: "50%",
-        //     left: "50%",
-        //     x: "-50%",
-        //     y: "-50%",
-        //     duration: 0,
-        //   })
-        //   .to(e.target, {
-        //     scale,
-        //     zIndex: 100,
-        //   });
-        console.log(e.target);
+    try {
+      let srcs = [];
+      for (let i of document.querySelectorAll(
+        ".react-photo-gallery--gallery img"
+      )) {
+        srcs.push(i.src);
+      }
+      const key = srcs.findIndex((x) => x === e.target.src);
+      this.setState({
+        lightboxSrcs: srcs,
+        lightboxActive: true,
+        lightboxKey: key,
       });
+    } catch (e) {
+      console.warn("Error opening image lightbox...", e);
     }
   }
-  closeLightbox() {
-    gsap.to(".lightboxBg", { opacity: 0, display: "none", duration: 0.5 });
-    gsap.fromTo(
-      ".lightboxImg",
-
-      {
-        top: "50vh",
-        left: "50vw",
-        x: "-50%",
-        y: "-50%",
-
-        duration: 0.25,
-      },
-      {
-        top: 0,
-        left: 0,
-        x: this.state.pos.left,
-        y: this.state.pos.top,
-      },
-      0
-    );
-    this.setState({ active: false });
-    gsap.to(body, { overflow: "auto" }, 0);
+  closeLightbox(e) {
+    this.setState({ lightboxActive: false });
   }
-
   render() {
     return (
-      <div className="mr4-l ml4-l pa2 b2 center">
+      <div id="reactGallery" className="mr4-l ml4-l pa2 b2 center">
         <div
           style={{ backdropFilter: "blur(5px)" }}
           className="lightboxBg dn z-2 fixed top-0 left-0 w-100 vh-100 bg-black-60 pointer"
           onClick={() => this.closeLightbox()}
         >
-          <img
-            className="lightboxImg"
-            src={this.state.src}
-            alt={this.state.src}
-          />
+          {this.state.lightboxActive ? (
+            <Lightbox
+              mainSrc={this.state.lightboxSrcs[this.state.lightboxKey]}
+              nextSrc={
+                this.state.lightboxSrcs[
+                  (this.state.lightboxKey + 1) % this.state.lightboxSrcs.length
+                ]
+              }
+              prevSrc={
+                this.state.lightboxSrcs[
+                  (this.state.lightboxKey +
+                    this.state.lightboxSrcs.length -
+                    1) %
+                    this.state.lightboxSrcs.length
+                ]
+              }
+              onCloseRequest={() => this.setState({ lightboxActive: false })}
+              onMovePrevRequest={() =>
+                this.setState({
+                  lightboxKey:
+                    (this.state.lightboxKey +
+                      this.state.lightboxSrcs.length -
+                      1) %
+                    this.state.lightboxSrcs.length,
+                })
+              }
+              onMoveNextRequest={() =>
+                this.setState({
+                  lightboxKey:
+                    (this.state.lightboxKey + 1) %
+                    this.state.lightboxSrcs.length,
+                })
+              }
+            />
+          ) : null}
         </div>
         <Gallery
           photos={this.state.gallerySettings}
