6eb21a178a7031768b0c7766ab077d21b2da1da5
unknown <etiennepousse@live.fr>
2021-04-29 17:30:27 +0200

    Solving issue with commit not up to date (new photos)

--- src/content/gallery_content.jsx
+++ src/content/gallery_content.jsx
@@ -1,74 +1,146 @@
 export const gallerySettings = [
   {
     src:
-      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_6.jpg?raw=true",
-    height: 1080,
-    width: 1440,
-  },
-  {
-    src:
-      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_4.jpg?raw=true",
-    height: 1080,
-    width: 1631,
-  },
-  {
-    src:
-      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_3.jpg?raw=true",
-    height: 1080,
-    width: 1620,
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_1-sm.jpg?raw=true",
+    height: 300,
+    width: 453,
   },
   {
     src:
       "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_1.jpg?raw=true",
-    height: 3264,
-    width: 4928,
+    height: 300,
+    width: 453,
   },
   {
     src:
-      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_5.jpg?raw=true",
-    height: 1080,
-    width: 1631,
-  },
-  {
-    src:
-      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_8.jpg?raw=true",
-    height: 1080,
-    width: 1631,
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_10-sm.jpg?raw=true",
+    height: 300,
+    width: 453,
   },
   {
     src:
       "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_10.jpg?raw=true",
-    height: 1080,
-    width: 1631,
+    height: 300,
+    width: 453,
   },
   {
     src:
-      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_9.jpg?raw=true",
-    height: 1080,
-    width: 1631,
-  },
-  {
-    src:
-      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_7.jpg?raw=true",
-    height: 945,
-    width: 2316,
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_11-sm.jpg?raw=true",
+    height: 300,
+    width: 453,
   },
   {
     src:
       "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_11.jpg?raw=true",
-    height: 3264,
-    width: 4928,
+    height: 300,
+    width: 453,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_12-sm.jpg?raw=true",
+    height: 300,
+    width: 400,
   },
   {
     src:
       "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_12.jpg?raw=true",
-    height: 960,
-    width: 1280,
+    height: 300,
+    width: 400,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_2-sm.jpg?raw=true",
+    height: 300,
+    width: 453,
   },
   {
     src:
       "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_2.jpg?raw=true",
-    height: 3264,
-    width: 4928,
+    height: 300,
+    width: 453,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_3-sm.jpg?raw=true",
+    height: 300,
+    width: 450,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_3.jpg?raw=true",
+    height: 300,
+    width: 450,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_4-sm.jpg?raw=true",
+    height: 300,
+    width: 453,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_4.jpg?raw=true",
+    height: 300,
+    width: 453,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_5-sm.jpg?raw=true",
+    height: 300,
+    width: 453,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_5.jpg?raw=true",
+    height: 300,
+    width: 453,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_6-sm.jpg?raw=true",
+    height: 300,
+    width: 400,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_6.jpg?raw=true",
+    height: 300,
+    width: 400,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_7-sm.jpg?raw=true",
+    height: 300,
+    width: 735,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_7.jpg?raw=true",
+    height: 300,
+    width: 735,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_8-sm.jpg?raw=true",
+    height: 300,
+    width: 453,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_8.jpg?raw=true",
+    height: 300,
+    width: 453,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_9-sm.jpg?raw=true",
+    height: 300,
+    width: 453,
+  },
+  {
+    src:
+      "https://github.com/captain-inch/lcc_export/blob/main/src/media/img/gallery/gallery_9.jpg?raw=true",
+    height: 300,
+    width: 453,
   },
 ];
